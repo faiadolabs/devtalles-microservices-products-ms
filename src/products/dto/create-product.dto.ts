c6e0db271e3a50f931a370b53d316ca74a4f89cf
Kike <kike@faiadolabs.com>
2026-03-31 17:45:45 +0000

    Entidad y DTOs

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+// Define, qué información, a la hora de crear un producto,
+
+import { Type } from "class-transformer";
+import { IsNumber, IsString, Min } from "class-validator";
+
+// estoy esperando.
+export class CreateProductDto {
+
+    @IsString()
+    public name:string;
+
+    @IsNumber({
+        maxDecimalPlaces: 4,
+    })
+    @Min(0)
+    @Type( () => Number )
+    public price:string;
+
+}
